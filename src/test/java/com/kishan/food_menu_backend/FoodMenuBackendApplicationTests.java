629213bf514200911d7592a544818dc248acebcf
krishna <kishanbaish@gmail.com>
2025-12-05 13:49:39 +0530

    Add Spring Boot Maven backend skeleton

---
+++ src/test/java/com/kishan/food_menu_backend/FoodMenuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kishan.food_menu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMenuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
